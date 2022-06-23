0c9dec7faf9cc481c931aaa0c3bd32c501f3ad9d
vv.chekurda <vv.chekurda@tensor.ru>
2022-06-23 16:44:32 +0300

    доработка аналитики

--- app/src/main/java/com/example/simpletextview/MainActivity.kt
+++ app/src/main/java/com/example/simpletextview/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.simpletextview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.simpletextview.custom_tools.utils.ExecutionTimeAnalytic
+import com.example.simpletextview.custom_tools.utils.dp
+import com.example.simpletextview.simple_tv.SimpleTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,15 +68,33 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.simple_text_view).setOnClickListener {
             ExecutionTimeAnalytic.analyzeExecutionTime(
                 count,
-                ExecutionTimeAnalytic.Execution("Without cache") {
+                ExecutionTimeAnalytic.Execution("Inflate") {
                     inflater.inflate(R.layout.simple_text_view_without_cache, rootView, false)
                 },
-                ExecutionTimeAnalytic.Execution("With cache") {
+                ExecutionTimeAnalytic.Execution("Inflate") {
                     inflater.inflate(R.layout.simple_text_view, rootView, false)
                 },
-                ExecutionTimeAnalytic.Execution {
+                ExecutionTimeAnalytic.Execution("Inflate") {
                     inflater.inflate(R.layout.app_compat_text_view, rootView, false)
-                }
+                },
+                ExecutionTimeAnalytic.Execution("Program") {
+                    SimpleTextView(this).apply {
+                        text = "SimpleTextView Some Text"
+                        paint.textSize = dp(20).toFloat()
+                        paint.color = Color.BLACK
+                        setPadding(dp(1), dp(1), dp(1), dp(1))
+                        maxLines = 1
+                    }
+                },
+                ExecutionTimeAnalytic.Execution("Program") {
+                    AppCompatTextView(this).apply {
+                        text = "AppCompatTextView Some Text"
+                        paint.textSize = dp(20).toFloat()
+                        paint.color = Color.BLACK
+                        setPadding(dp(1), dp(1), dp(1), dp(1))
+                        maxLines = 1
+                    }
+                },
             ).also(::showResult)
         }
     }
