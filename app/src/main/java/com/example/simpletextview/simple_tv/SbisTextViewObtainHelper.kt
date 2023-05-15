7eeb93cf6c343c51d16d8da3abb7f2d91b20c46b
vv.chekurda <vv.chekurda@tensor.ru>
2023-05-15 13:45:52 +0300

    фикс работы textAppearance

--- app/src/main/java/com/example/simpletextview/simple_tv/SbisTextViewObtainHelper.kt
+++ app/src/main/java/com/example/simpletextview/simple_tv/SbisTextViewObtainHelper.kt
@@ -11,21 +11,11 @@ import androidx.annotation.StyleableRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.content.withStyledAttributes
+import com.example.simpletextview.R
 
 object SbisTextViewObtainHelper {
 
     private var textAppearanceCache = hashMapOf<Int, TextAppearanceData>()
-
-    private val textAppearanceAttrs by lazy(LazyThreadSafetyMode.NONE) {
-        intArrayOf(
-            android.R.attr.textSize,
-            android.R.attr.textColor,
-            android.R.attr.textColorLink,
-            android.R.attr.textAllCaps,
-            android.R.attr.textStyle,
-            android.R.attr.fontFamily
-        )
-    }
 
     internal fun getTextAppearance(
         context: Context,
@@ -34,22 +24,21 @@ object SbisTextViewObtainHelper {
     ): TextAppearanceData {
         val cachedStyle = textAppearanceCache[style]
         return if (cachedStyle == null) {
-            fun Int.index(): Int = textAppearanceAttrs.indexOf(this)
             var data: TextAppearanceData? = null
-            context.withStyledAttributes(attrs = textAppearanceAttrs, resourceId = style) {
-                val textSize = getDimensionPixelSize(android.R.attr.textSize.index(), 0)
+            context.withStyledAttributes(resourceId = style, attrs = R.styleable.SbisTextView) {
+                val textSize = getDimensionPixelSize(R.styleable.SbisTextView_android_textSize, 0)
                     .takeIf { it != 0 }
-                val colorStateList = getColorStateList(context, this, android.R.attr.textColor.index())
+                val colorStateList = getColorStateList(context, this, R.styleable.SbisTextView_android_textColor)
                 val color = colorStateList?.defaultColor
-                    ?: getColor(android.R.attr.textColor.index(), NO_RESOURCE)
+                    ?: getColor(R.styleable.SbisTextView_android_textColor, NO_RESOURCE)
                         .takeIf { it != NO_RESOURCE }
-                val linkColorStateList = getColorStateList(context, this, android.R.attr.textColorLink.index())
-                val fontFamily = getResourceId(android.R.attr.fontFamily.index(), NO_RESOURCE)
+                val linkColorStateList = getColorStateList(context, this, R.styleable.SbisTextView_android_textColorLink)
+                val fontFamily = getResourceId(R.styleable.SbisTextView_android_fontFamily, NO_RESOURCE)
                     .takeIf { it != NO_RESOURCE }
-                val textStyle = getInt(android.R.attr.textStyle.index(), NO_RESOURCE)
+                val textStyle = getInt(R.styleable.SbisTextView_android_textStyle, NO_RESOURCE)
                     .takeIf { it != NO_RESOURCE }
                 val typeface = getTypeface(context, currentTypeface, fontFamily, textStyle)
-                val allCaps = getBoolean(android.R.attr.textAllCaps.index(), false)
+                val allCaps = getBoolean(R.styleable.SbisTextView_android_textAllCaps, false)
 
                 data = TextAppearanceData(
                     textSize = textSize?.toFloat(),
